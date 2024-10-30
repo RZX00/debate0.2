95cceb28a27d312cb3b27254e8d831aafb434d95
RZX00 <114714454+RZX00@users.noreply.github.com>
2024-10-30 11:14:06 +0800

    Create mongodb.js

---
+++ api/mongodb.js
@@ -0,0 +1,25 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = "mongodb+srv://xyueli166:<db_password>@cluster0.w6gwt.mongodb.net/";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("debate20").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
