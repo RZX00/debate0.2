db3498703aa492e286ebb589c314575a034b4c75
RZX00 <114714454+RZX00@users.noreply.github.com>
2024-11-02 09:07:42 +0800

    Update UpgradeModal.tsx
    012

--- src/components/UpgradeModal.tsx
+++ src/components/UpgradeModal.tsx
@@ -61,12 +61,10 @@ const UpgradeModal: React.FC<UpgradeModalProps> = ({ onClose }) => {
         </div>
         {!showThankYou ? (
           <>
-            <p className="mb-6">升级高级方案以体验不限次数的深度搜索功能与AI磨辩功能。（2.69RMB/月 or 0.1RMB/3次深度搜索</p>
-            <p className="mb-6">升级高级方案
-亲爱的各位先行者们：内测期间，我们“打骨折价”的“高级方案”是创始人奉上的专属福利！也希望大家能畅享不限次数、独一无二的深度搜索功能与AI磨辩功能。
-
+            <p className="mb-6">升级高级方案以体验不限次数的深度搜索功能与AI磨辩功能。</p>
+            <span className="block mt-1 font-semibold text-blue-600">仅需2.69RMB/月 or 0.1/3次深度搜索</span>
+            <p className="mb-6">亲爱的各位先行者们：内测期间，我们“打骨折价”的“高级方案”是创始人奉上的专属福利！也希望大家能畅享不限次数、独一无二的深度搜索功能与AI磨辩功能。
 深度搜索将增加学术搜索和专业数据搜索（如相关法案、实验报告、经济数据等专业数据来源），并使用【专业级辩论大模型】提供更丰富全面细致的回答。
-
 机不可失，时不再来，内测结束后我们将恢复原价，同时发放首批高级付费用户专属福利！</p>
             <div className="flex justify-end space-x-4">
               <button
@@ -85,7 +83,9 @@ const UpgradeModal: React.FC<UpgradeModalProps> = ({ onClose }) => {
           </>
         ) : (
           <p className="text-green-600 font-semibold">
-            wow！没想到你真的愿意付费体验我们的功能！非常感谢您的支持！我们会全力开发该功能！一旦上线，我们将立即通知您。期待为您带来更优质的体验！作为我们的补偿，该功能正式上线后您将获得免费体验次数！再次感谢！
+            wow！没想到你真的愿意付费体验我们的功能！非常感谢您的支持！我们会全力开发该功能！一旦上线，我们将立即通知您。期待为您带来更优质的体验！</p>
+             <p className="text-green-600 font-semibold">
+            作为我们的补偿，该功能正式上线后您将获得免费体验次数！再次感谢！
           </p>
         )}
       </div>
